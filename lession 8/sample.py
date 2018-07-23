9a1fa227ed679b89d4161b7641cfd65e0f6ed58c
Hung Nhat <hung.nhat@huntermacdonald.com>
2018-07-23 07:12:46 +0100

    Lesson 8: Algorithm Evaluation Metrics

---
+++ lession 8/sample.py
@@ -0,0 +1,16 @@
+from pandas import read_csv
+from sklearn.model_selection import KFold
+from sklearn.model_selection import cross_val_score
+from sklearn.linear_model import LogisticRegression
+
+url = "https://raw.githubusercontent.com/jbrownlee/Datasets/master/pima-indians-diabetes.data.csv"
+names = ['preg', 'plas', 'pres', 'skin', 'test', 'mass', 'pedi', 'age', 'class']
+dataframe = read_csv(url, names=names)
+array = dataframe.values
+X = array[:,0:8]
+Y = array[:,8]
+kfold = KFold(n_splits=3, random_state=7)
+model = LogisticRegression()
+scoring = 'neg_log_loss'
+result = cross_val_score(model, X, Y, cv=kfold, scoring=scoring)
+print("Logloss: %.3f (%.3f)" % (result.mean(), result.std()))
